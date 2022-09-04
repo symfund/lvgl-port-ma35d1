62fc881b6b59071d763134ec1400b8830056cd67
江天文 <symfund@outlook.com>
2022-09-04 18:28:23 +0800

    add launcher in title bar

--- qemu/qemu.bat
+++ qemu/qemu.bat
@@ -1,2 +1,2 @@
 PATH=%~dp0;C:\Program Files\qemu;%PATH%
-qemu-system-x86_64.exe -M pc -kernel bzImage -drive file=rootfs.ext2,if=virtio,format=raw -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net user -device virtio-vga,max_outputs=2 --full-screen
+qemu-system-x86_64.exe -M pc -kernel bzImage -drive file=rootfs.ext2,if=virtio,format=raw -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net user -device virtio-gpu,max_outputs=2 --full-screen
